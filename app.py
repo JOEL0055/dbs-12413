738c903e698310be177c6cd2a3db2e37a48e28f3
JOEL0055 <JOEL0055@e.ntu.edu.sg>
2024-08-19 16:11:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
-app = Flask(__name__)
-from flask import request, render_template
-@app.route("/", methods = ["GET", "POST"])
-def i():
-    if request.method == "POST":
-        num = float(request.form.get("rates"))
-        return(render_template("index.html", result = 90.2-(50.6*num)))
-    else:
-        return render_template("index.html", result ="Waiting……….")
-if __name__=="__main__":
+from flask import Flask, render_template, request
+import google.generativeai as palm
+
+api = "AIzaSyCaQcgKn95ZO6AR1t2PXzk9UydTkt4sWZQ"
+palm.configure(api_key=api)
+model = {"model": "models/chat-bison-001"}
+
+app = Flask(__name__, template_folder=".")
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    return(render_template("index.html"))
+
+@app.route("/financial_FAQ",methods=["GET","POST"])
+def financial_FAQ():
+    return(render_template("financial_FAQ.html"))
+
+@app.route("/makersuite",methods=["GET","POST"])
+def makersuite():
+    q = request.form.get("q")
+    r = palm.chat(messages=q, **model)
+    return(render_template("makersuite.html",r=r.last))
+
+if __name__ == "__main__":
     app.run()
