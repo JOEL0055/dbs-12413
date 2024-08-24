fd58aeeed265f210a717a8d401f7b6b9001bcaf6
JOEL0055 <JOEL0055@e.ntu.edu.sg>
2024-08-24 19:25:54 +0800

    app.py

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
 from flask import Flask, render_template, request
-import google.generativeai as palm
 
-api = "AIzaSyCaQcgKn95ZO6AR1t2PXzk9UydTkt4sWZQ"
-palm.configure(api_key=api)
-model = {"model": "models/chat-bison-001"}
+app = Flask(__name__, template_folder="/Users/joelaiweithai/Downloads/BC3411/html/html_script")
 
-app = Flask(__name__, template_folder=".")
-
-@app.route("/",methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return(render_template("index.html"))
-
-@app.route("/financial_FAQ",methods=["GET","POST"])
-def financial_FAQ():
-    return(render_template("financial_FAQ.html"))
-
-@app.route("/makersuite",methods=["GET","POST"])
-def makersuite():
-    q = request.form.get("q")
-    r = palm.chat(messages=q, **model)
-    return(render_template("makersuite.html",r=r.last))
+    return render_template("finance.html")
 
 if __name__ == "__main__":
-    app.run()
+    # Specify a different port
+    app.run(port=5001)
